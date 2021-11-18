630eee7531c13b8599db76eca9fa8030935c0726
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-18 18:42:10 +0900

    インターバル修正確認

--- server/modules/intervalRequest.ts
+++ server/modules/intervalRequest.ts
@@ -3,22 +3,23 @@ import axios from "axios";
 export const intervalRequest = () => {
   const currentHour = new Date().getHours();
   const currentMinute = new Date().getMinutes();
-  let interval: ReturnType<typeof setTimeout> | null = null;
+  // let interval: ReturnType<typeof setTimeout> | null = null;
 
   const request = async () => {
     return axios.get("https://heroku-tutorial-2021.herokuapp.com/");
   };
 
   // // 毎夜23:59でインターバル終了
-  setTimeout(() => {
-    if (interval) {
-      clearInterval(interval);
-    }
-  }, new Date().setHours(23, 59, 0, 0) - new Date().setHours(currentHour, currentMinute));
+  // setTimeout(() => {
+  //   if (interval) {
+  //     clearInterval(interval);
+  //   }
+  // }, new Date().setHours(23, 59, 0, 0) - new Date().setHours(currentHour, currentMinute));
 
   // 毎朝7時からインターバルスタート
   setTimeout(() => {
     // herokuの仕様で30分リクエストがないとスリープしてしまうため30分ごとにリクエスト送信
-    interval = setInterval(request, 60000 * 30);
+    // interval =
+    setInterval(request, 60000 * 30);
   }, new Date().setHours(7, 0, 0, 0) - new Date().setHours(currentHour, currentMinute));
 };
