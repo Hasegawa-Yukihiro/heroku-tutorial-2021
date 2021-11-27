b39a90ef958e33419e5a0577b401062b74066240
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-27 16:36:36 +0900

    port確認

--- client/webpack/webpack.config.prod.js
+++ client/webpack/webpack.config.prod.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const baseConfig = require("./webpack.config.base.js");
 const { merge } = require("webpack-merge");
 
 module.exports = merge(baseConfig, {
-  mode: "production"
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js",
+    publicPath: "./"
+  }
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,7 @@ let htmlFile = fs.readFileSync(htmlFilePathname, { encoding: "utf-8" });
 app.get(/^(?!.*api).+$/, (req, res) => res.status(200).send(htmlFile));
 
 const port = process.env.PORT || 3000;
+console.log("success-port", port);
 app.listen(port);
 
 intervalRequest();
